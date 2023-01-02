6aa756a7b8f2bbc5a115483533fbef3feecb2e8c
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2023-01-02 18:47:44 -0300

    ajusta tabelas

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,7 +1,10 @@
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
+
+[Table("Categorias")]
 
 public class Categoria
 {
@@ -10,8 +13,16 @@ public class Categoria
         Produtos = new Collection<Produto>();
     }
 
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
+
     public ICollection<Produto>? Produtos { get; set; }
 }
