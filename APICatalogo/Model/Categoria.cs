b5e51b5cc180e3df8c0e2392a87b8f916550348b
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2023-01-02 17:03:57 -0300

    add models

---
+++ APICatalogo/Model/Categoria.cs
@@ -0,0 +1,8 @@
+namespace APICatalogo.Model;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
