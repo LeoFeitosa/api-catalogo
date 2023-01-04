b5f8d9023514a56e197565fcb1fb17edcc8e8565
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2023-01-03 23:45:24 -0300

    adiciona controller Categorias

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,87 @@
+using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            context = _context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = this._context.Categorias.ToList();
+            if (categorias is null)
+            {
+                return NotFound("Categorias nao encontradas");
+            }
+
+            return categorias;
+
+        }
+
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = this._context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound("Categoria nao encontrada");
+            }
+
+            return categoria;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria is null)
+            {
+                return BadRequest();
+            }
+
+            this._context.Categorias.Add(categoria);
+            this._context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            this._context.Entry(categoria).State = EntityState.Modified;
+            this._context.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var Categoria = this._context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            if (Categoria is null)
+            {
+                return NotFound("Categoria nao encontrada");
+            }
+
+            this._context.Categorias.Remove(Categoria);
+            this._context.SaveChanges();
+
+            return Ok(Categoria);
+        }
+    }
+}
