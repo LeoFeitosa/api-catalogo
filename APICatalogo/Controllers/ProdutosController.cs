f132b3a2fafd6f7f1678344a5ab83399cf74302d
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2023-01-03 22:34:32 -0300

    adiciona controller produtos

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using APICatalogo.Context;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
