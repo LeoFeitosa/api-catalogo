d7b26039d66a2e65299391f0fc94fced61b7a35b
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2023-01-03 22:18:26 -0300

    popula tabela Produtos

---
+++ APICatalogo/Migrations/20230104011048_PopulaProdutos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                "Values ('Dolly', 'Refrigerante refrescante 2litros', 5.65, 'dolly.jpg', 300, now(), 1)");
+
+            mb.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                "Values ('Melancia', 'Fruta grande', 4.50, 'melancia.jpg', 140, now(), 2)");
+
+            mb.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                "Values ('Camiseta', 'Camiseta azul marinho', 12.99, 'camiseta.jpg', 50, now(), 3)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
